b793c94ade89e42e421eba47a5fc0fa7e17e6746
Kubice <24kub_yt@mail.ru>
2025-12-27 13:37:27 +0500

    Создание простой HTML страницы

---
+++ LB4/fullcontent.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta property="og:image" content="https://i.pinimg.com/1200x/a1/31/80/a131807d68c577dec33901b2c65653c9.jpg">
+    <meta property="og:description" content="Крутая библиотека для чтения книг и комиксов и еще чего-то причем ОНЛААЙН. Читай свои книги или если не хочешь читай чужие книги">
+    <meta property="og:title" content="Библи0тека">
+
+    <title>Библи0тека</title>
+    <!-- <title>8i8L10t3ka</title> -->
+    <link rel="icon" href="src/logo.jpg">
+    
+    <link rel="stylesheet" href="./sltyles.css">
+</head>
+<body>
+
+<header class="header">
+    <div class="logo">Библи0тека</div>
+    <nav class="nav">
+        <a href="#">Читатели</a>
+        <a href="#">Книги</a>
+    </nav>
+</header>
+
+<main class="content">
+    <section class="welcome">
+        <h1>Добро пожаловать в электронную библи0теку</h1>
+        <p>
+            Здесь Вы можете читать чужие книги
+            или публиковать свои, чтобы чужие люди читали их
+        </p>
+        <button class="btn">Начать работу</button>
+    </section>
+</main>
+
+<footer class="footer">
+    <p>© <?= date('Y') ?> Bibli0teka </p>
+    <p>Контакты: casino.library.support@example.com | +7 (777) 777-77-77</p>
+</footer>
+
+</body>
+</html>
---
+++ LB4/index.php
@@ -0,0 +1,3 @@
+<?php
+
+require('fullcontent.php');
